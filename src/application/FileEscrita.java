b170e4eaa4870770a2fabb95e85d98157a6d4006
Lucas Vale <lucasvale1998.lv@gmail.com>
2020-01-07 20:29:48 -0300

    FileWriter e BufferedWriter

---
+++ src/application/FileEscrita.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileEscrita {
+	public static void main(String[] args) {
+		String[] lines = new String[] {
+				"Goo Morning",
+				"Good afternoon",
+				"Good Night"
+		};
+		
+		String path = "C:\\Auto Estudos\\18 - Java COMPLETO 2020 Programação Orientada a Objetos +Projetos\\Seção 17 Trabalhando com arquivos\\out.txt";
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for (String line: lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
